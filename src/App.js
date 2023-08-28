f1997a25a0a2f068e8b41989ffa7e487a3c8e3c3
Saim Amir <saimxamir@gmail.com>
2023-08-28 18:30:04 -0400

    update about and home routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+
 
 function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path="/" element={<div>Home Page</div>}/>
-    
-    <Route path="testing" element={
-    <div>
-      <h2>Testing</h2>
-      </div>} />
-
+    <Route path='/' element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='products' element={<Products />} />
   </Routes>
     </BrowserRouter>
 }
