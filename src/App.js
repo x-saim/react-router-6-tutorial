f430fed3fe51371dbafd798400725a39d308131f
Saim Amir <saimxamir@gmail.com>
2023-08-28 19:41:01 -0400

    utilize useParams and set up SingleProduct comp

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from './pages/About';
 import Products from './pages/Products';
 import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
+import SingleProduct from './pages/SingleProduct';
+
 function App() {
   return (
   
@@ -14,6 +16,8 @@ function App() {
         <Route index element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='products' element={<Products />} />
+        <Route path='products/:productId' element=
+        {<SingleProduct />} />
         <Route path='*' element={<Error />} />
       </Route>
     </Routes>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,9 +1,15 @@
+import { Link, useParams } from 'react-router-dom';
+import products from '../data';
+
 const SingleProduct = () => {
+  const { productId } = useParams();
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>{productId}</h2>
+      <Link to='/products'>back to products</Link>
     </section>
-  );
+    );
 };
 
 export default SingleProduct;
