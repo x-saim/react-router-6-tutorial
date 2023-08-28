9f2c594f3e0a46997462b8c963e4fbce024ab0b2
Saim Amir <saimxamir@gmail.com>
2023-08-28 18:14:16 -0400

    add react router and set up initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<div>Home Page</div>}/>
+    
+    <Route path="testing" element={
+    <div>
+      <h2>Testing</h2>
+      </div>} />
+
+  </Routes>
+    </BrowserRouter>
 }
 
 export default App;
