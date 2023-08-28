35d71e35c0c146f5601d362c3ed30d3aa2d8602e
Saim Amir <saimxamir@gmail.com>
2023-08-28 18:48:16 -0400

    update home page with Outlet

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import Products from './pages/Products';
 import Error from './pages/Error';
 
 function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='about' element={<About />} />
-    <Route path='products' element={<Products />} />
-    <Route path='*' element={<Error />} />
-
- 
-  </Routes>
-    </BrowserRouter>
+  return (
+  
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}>
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+        <Route path='*' element={<Error />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  );
 }
 
 export default App;
