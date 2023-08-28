8893c547a80a0b9d21276b69da6c52a67e10d543
Saim Amir <saimxamir@gmail.com>
2023-08-28 19:46:48 -0400

    update products comp to dynamically render

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,28 @@
+import { Link } from 'react-router-dom';
+import products from '../data'
+
 const Products = () => {
+
+  const productList = products.map((product) => {
+    return (
+      <article key={product.id}>
+        <h5>{product.name}</h5>
+        <Link to={`/products/${product.id}`}>
+          more info </Link>
+      </article>
+    )
+  })
+
+
   return (
-    <>
-      <section className='section'>
-        <h2>products</h2>
-      </section>
-    </>
+    <section className='section'>
+      <h2>products</h2>
+
+      <div className='products'>
+      {productList}
+      </div>
+    </section>
+
   );
 };
 
