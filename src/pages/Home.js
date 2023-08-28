d7c7c4d86a4990cdf173a03a72895a7b8f80247e
Saim Amir <saimxamir@gmail.com>
2023-08-28 18:53:26 -0400

    add navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import { Link, Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 
 const Home = () => {
   return (
-    <section className='section'>
-      <h2>Home Page</h2>
-    <Outlet />
-    </section>
+    <>
+     <Navbar />
+      <section className='section'>
+      <Outlet />
+      </section>
+    </>
+   
   );
 };
 export default Home;
